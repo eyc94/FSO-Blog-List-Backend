3e6e956e93b67f63132760d6672f30ef95190b69
Eric Chin <ericychin94@gmail.com>
2022-01-14 14:38:20 -0800

    Removed commented code

--- index.js
+++ index.js
@@ -39,13 +39,6 @@ app.get('/api/blogs', (request, response) => {
 app.post('/api/blogs', (request, response) => {
     const blog = new Blog(request.body);
 
-    // const blog = new Blog({
-    //     title: body.title,
-    //     author: body.author,
-    //     url: body.url,
-    //     likes: body.likes
-    // });
-
     blog
         .save()
         .then(result => {
