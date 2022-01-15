6ab2697270eef727f075d5eb1c4a23067dbc135b
Eric Chin <ericychin94@gmail.com>
2022-01-15 01:00:05 -0800

    Made more appealing

--- tests/listHelper.test.js
+++ tests/listHelper.test.js
@@ -142,13 +142,15 @@ describe('favorite blog', () => {
 
     test('of all blogs', () => {
         const result = listHelper.favoriteBlog(listWithManyBlogs);
-        expect(result).toEqual({
-            _id: "5a422b3a1b54a676234d17f9",
-            title: "Canonical string reduction",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-            likes: 12,
-            __v: 0
-        });
+        expect(result).toEqual(
+            {
+                _id: "5a422b3a1b54a676234d17f9",
+                title: "Canonical string reduction",
+                author: "Edsger W. Dijkstra",
+                url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+                likes: 12,
+                __v: 0
+            }
+        );
     });
 });
