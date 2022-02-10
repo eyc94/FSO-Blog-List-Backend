e34d0bbee19523260f6bce23471a1412382806f4
Eric Chin <ericychin94@gmail.com>
2022-02-09 18:12:50 -0800

    Added hard-coded blogs to initialize DB

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,21 +4,29 @@ const app = require('../app');
 
 const api = supertest(app);
 
-// const Blog = require('../models/blog');
-// const initialBlogs = [
-//     {
-//         title: 'This is a sample title 1',
-//         author: 'John Doe',
-//         url: 'www.sample-one.com',
-//         likes: 20
-//     },
-//     {
-//         title: 'This is a sample title 2',
-//         author: 'Jane Doe',
-//         url: 'www.sample-two.com',
-//         likes: 35
-//     }
-// ];
+const Blog = require('../models/blog');
+const initialBlogs = [
+    {
+        title: 'This is a sample title 1',
+        author: 'John Doe',
+        url: 'www.sample-one.com',
+        likes: 20
+    },
+    {
+        title: 'This is a sample title 2',
+        author: 'Jane Doe',
+        url: 'www.sample-two.com',
+        likes: 35
+    }
+];
+
+beforeEach(async () => {
+    await Blog.deleteMany({});
+    let blogObject = new Blog(initialBlogs[0]);
+    await blogObject.save();
+    blogObject = new Blog(initialBlogs[1]);
+    await blogObject.save();
+});
 
 test('blogs are returned as json', async () => {
     await api
