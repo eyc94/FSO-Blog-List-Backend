f78bc1fef05f72b184c1fcba0c23b5c8affe0fef
Eric Chin <ericychin94@gmail.com>
2022-02-09 17:56:32 -0800

    Added test for fetching all blogs

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,6 +4,27 @@ const app = require('../app');
 
 const api = supertest(app);
 
+// const Blog = require('../models/blog');
+// const initialBlogs = [
+//     {
+//         title: 'This is a sample title 1',
+//         author: 'John Doe',
+//         url: 'www.sample-one.com',
+//         likes: 20
+//     },
+//     {
+//         title: 'This is a sample title 2',
+//         author: 'Jane Doe',
+//         url: 'www.sample-two.com',
+//         likes: 35
+//     }
+// ];
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs');
+    expect(response.body).toHaveLength(2);
+});
+
 afterAll(() => {
     mongoose.connection.close();
 });
