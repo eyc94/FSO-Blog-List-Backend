f057dbd4bcabd103c5e8210caff7faf043863ac4
Eric Chin <ericychin94@gmail.com>
2022-02-10 01:11:01 -0800

    Added PUT route implementation with async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -22,4 +22,18 @@ blogsRouter.delete('/:id', async (request, response, next) => {
     response.status(204).end();
 });
 
+blogsRouter.put('/:id', async (request, response, next) => {
+    const body = request.body;
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    };
+
+    const updatedNote = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+    response.json(updatedNote);
+});
+
 module.exports = blogsRouter;
