047b7b755ec06496fa334c99a08b4741a0f29c14
Eric Chin <ericychin94@gmail.com>
2022-02-10 00:52:49 -0800

    Fixed typo

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -19,7 +19,7 @@ blogsRouter.post('/', async (request, response) => {
 });
 
 blogsRouter.delete('/:id', async (request, response, next) => {
-    await blog.findByIdAndRemove(request.params.id);
+    await Blog.findByIdAndRemove(request.params.id);
     response.status(204).end();
 });
 
