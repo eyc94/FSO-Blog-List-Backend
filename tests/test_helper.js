d294ce6047df062f209a0d32b5d720f446341bd3
Eric Chin <ericychin94@gmail.com>
2022-02-09 18:48:32 -0800

    Added function in helper module to return blogs as JSON

---
+++ tests/test_helper.js
@@ -0,0 +1,25 @@
+const Blog = require('../models/blog');
+
+const initialBlogs = [
+    {
+        title: 'This is a sample title 1',
+        author: 'John Doe',
+        url: 'www.sample-one.com',
+        likes: 20
+    },
+    {
+        title: 'This is a sample title 2',
+        author: 'Jane Doe',
+        url: 'www.sample-two.com',
+        likes: 35
+    }
+];
+
+const blogsInDb = async () => {
+    const blogs = await Blog.find({});
+    return blogs.map(blog => blog.toJSON());
+};
+
+module.exports = {
+    blogsInDb
+};
