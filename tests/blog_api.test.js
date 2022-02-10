528ef19ed886a12a0bcec9594396ba1d78697d65
Eric Chin <ericychin94@gmail.com>
2022-02-09 17:05:14 -0800

    Added test init statements

---
+++ tests/blog_api.test.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+
+const api = supertest(app);
+
+afterAll(() => {
+    mongoose.connection.close();
+});
