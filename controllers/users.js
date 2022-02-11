0930fdad907060ba650de8ce9952778f2513cb52
Eric Chin <ericychin94@gmail.com>
2022-02-11 13:35:04 -0800

    Fixed typo

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 usersRouter.post('/', async (request, response) => {
     const { username, name, password } = request.body;
 
-    const existingUser = await user.findOne({ username });
+    const existingUser = await User.findOne({ username });
     if (existingUser) {
         return response.status(400).json({ error: 'username must be unique' });
     }
