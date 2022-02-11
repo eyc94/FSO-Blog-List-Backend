49a385ba63ab6d1d9508c4e0822307054e2df828
Eric Chin <ericychin94@gmail.com>
2022-02-11 14:48:14 -0800

    Adding blogs with first user

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({});
@@ -7,13 +8,23 @@ blogsRouter.get('/', async (request, response) => {
 });
 
 blogsRouter.post('/', async (request, response) => {
+    const body = request.body;
+
+    const users = await User.find({});
+    const user = users[0];
+
     const blog = new Blog({
-        title: request.body.title,
-        author: request.body.author,
-        url: request.body.url,
-        likes: request.body.likes || 0
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes || 0,
+        user: user._id
     });
     const savedBlog = await blog.save();
+
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
     response.json(savedBlog);
 });
 
